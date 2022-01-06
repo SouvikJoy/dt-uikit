e6fd03390e2159559282ec27e4c539f35e5862fc
Souvik Saha Joy <souviksahajoy@gmail.com>
2022-01-07 04:16:17 +0600

    init v4

--- src/components/components.js
+++ src/components/components.js
@@ -1,20 +1,20 @@
-import dtAccordion from "@/components/dtAccordion.vue";
-import dtAccordionItem from "@/components/dtAccordionItem.vue";
+import dtAccordion from "../components/dtAccordion.vue";
+import dtAccordionItem from "../components/dtAccordionItem.vue";
 
-import dtButton from "@/components/dtButton.vue";
+import dtButton from "../components/dtButton.vue";
 
-import dtNavbar from "@/components/dtNavbar.vue";
-import dtNavbarBrand from "@/components/dtNavbarBrand.vue";
-import dtNavbarNav from "@/components/dtNavbarNav.vue";
-import dtCollapse from "@/components/dtCollapse.vue";
-import dtNavbarItem from "@/components/dtNavbarItem.vue";
-import dtNavbarToggler from "@/components/dtNavbarToggler.vue";
+import dtNavbar from "../components/dtNavbar.vue";
+import dtNavbarBrand from "../components/dtNavbarBrand.vue";
+import dtNavbarNav from "../components/dtNavbarNav.vue";
+import dtCollapse from "../components/dtCollapse.vue";
+import dtNavbarItem from "../components/dtNavbarItem.vue";
+import dtNavbarToggler from "../components/dtNavbarToggler.vue";
 
-import dtContainer from "@/components/dtContainer.vue";
-import dtRow from "@/components/dtRow.vue";
-import dtCol from "@/components/dtCol.vue";
+import dtContainer from "../components/dtContainer.vue";
+import dtRow from "../components/dtRow.vue";
+import dtCol from "../components/dtCol.vue";
 
-import dtIcon from "@/components/dtIcon.vue";
+import dtIcon from "../components/dtIcon.vue";
 
 export default {
   dtAccordion,
